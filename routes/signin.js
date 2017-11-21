bfa8dcae6044a20f90f9629b07366eacfdc8c3cc
Sawyer-li <13894705977@163.com>
2017-11-21 09:48:23 +0800

    登入登出系统跑通

--- routes/signin.js
+++ routes/signin.js
@@ -9,8 +9,38 @@ router.get('/', function(req, res, next){
 	res.render('signin');
 })
 
-router.post('post', checkNotLogin, function(req, res, next){
+//checkNotLogin,
+router.post('/',  function(req, res, next){
 	const name = req.fields.name;
 	const password = req.fields.password;
+	console.log(name,password);
+	try {
+		if(!name.length){
+			throw new Error('请填写用户名');
+		}
+		if(!password.length){
+			throw new Error('填写密码');
+		}
+	}catch(e){
+		req.flash('error',e.message);
+		return res.redirect('back');
+	}
+
+	UserModel.getUserByname(name)
+		.then(function(user){
+			if(!user){
+				req.flash('error','用户名不存在');
+				return res.redirect('back');
+			}
+			if(sha1(password) !== user.password){
+				req.flash('error','密码不正确');
+				return res.redirect('back');
+			}
+			req.flash('success','登入成功');
+			delete user.password;
+			req.session.user = user;
+			res.redirect('/posts');
+		})
 })
+
 module.exports = router;
--- routes/signout.js
+++ routes/signout.js
@@ -4,10 +4,11 @@ const router = express.Router()
 const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /signout 登出
-router.get('/', checkLogin, function (req, res, next) {
+//checkLogin,
+router.get('/',  function (req, res, next) {
 	req.session.user = null;
 	req.flash('success','成功登入');
-	res.redirect('/post');
+	res.redirect('/posts');
 })
 
 module.exports = router
