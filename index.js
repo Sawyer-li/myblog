edc758f53fcc1010ade947feb38f5304c0393a34
lisongye <13894705977@163.com>
2017-12-04 22:11:13 +0800

    创建日志及其gitignore

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const config = require('config-lite')(__dirname);
 const routes = require('./routes');
 const pkg = require('./package')
 const app = express();
+const winston = require('winston')
+const expressWinston = require('express-winston')
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,66 +27,18 @@ app.use(session({
         url: config.mongodb // mongodb 地址
     })
 }))
-/************************** 在线系统 ***************************/
-//在线用户
-var onlineUsers = {};
-//当前在线人数
-var onlineCount = 0;
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    //监听新用户加入
-    socket.on('login', function(obj) {
-        //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
-        socket.name = obj.userid;
-        //检查在线列表，如果不在里面就加入
-        if (!onlineUsers.hasOwnProperty(obj.userid)) {
-            onlineUsers[obj.userid] = obj.username;
-            //在线人数+1
-            onlineCount++;
-        }
-        //向所有客户端广播用户加入
-        io.emit('login', {
-            onlineUsers: onlineUsers,
-            onlineCount: onlineCount,
-            user: obj
-        });
-        console.log(obj.username + '加入了聊天室');
-    });
-    //监听用户退出
-    socket.on('disconnect', function() {
-        //将退出的用户从在线列表中删除
-        if (onlineUsers.hasOwnProperty(socket.name)) {
-            //退出用户的信息
-            var obj = {
-                userid: socket.name,
-                username: onlineUsers[socket.name]
-            };
-            //删除
-            delete onlineUsers[socket.name];
-            //在线人数-1
-            onlineCount--;
-            //向所有客户端广播用户退出
-            io.emit('logout', {
-                onlineUsers: onlineUsers,
-                onlineCount: onlineCount,
-                user: obj
-            });
-            console.log(obj.username + '退出了聊天室');
-        }
-    });
-    //监听用户发布聊天内容
-    socket.on('message', function(obj) {
-        //向所有客户端广播发布的消息
-        io.emit('message', obj);
-        console.log(obj.username + '说：' + obj.content);
-    });
-});
-/************************** end 在线系统 ***************************/
+
 //form中图片上传路径
 app.use(require('express-formidable')({
     uploadDir: path.join(__dirname, 'public/img'),
     keepExtensions: true
 }))
+//错误打印
+app.use(function(err, req, res, next){
+    console.error(err);
+    req.flash('error',err.message)
+    res.redirect('post');
+})
 // flash 中间件，用来显示通知
 app.use(flash());
 app.locals.blog = {
@@ -99,14 +51,38 @@ app.use(function(req, res, next) {
     res.locals.error = req.flash('error').toString()
     next()
 })
-routes(app);
-
+// 正常请求的日志
+app.use(expressWinston.logger({
+  transports: [
+    new (winston.transports.Console)({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/success.log'
+    })
+  ]
+}))
+// 路由
+routes(app)
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })
+  ]
+}))
 /*io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });*/
-http.listen(config.port, function() {
+app.listen(config.port, function() {
     console.log('listen to 3000');
 })
